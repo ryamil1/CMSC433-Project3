f7daf2a3d6c784f93d097d75189b8404f4db9402
castlek1 <31139507+castlek1@users.noreply.github.com>
2019-07-09 20:43:58 -0400

    Resized Canvas

--- pro3_gui.js
+++ pro3_gui.js
@@ -1,13 +1,18 @@
 var tID; //we will use this variable to clear the setInterval()
-function animateScript($speed) {
-	var    position = 0; //start position for the image slicer
-	const  interval = 300/$speed;
+var tID2;
 
+// Speeds
+// 1 - Steady
+// 1.5 - Strenuous
+// 2 - Grueling
+function animateScript(speed) {
+	var    position = 0; //start position for the image slicer
+	const  interval = 300/speed;
+	
 	tID = setInterval ( () => {
-		console.log(position);
 		var ctx = (document.getElementById("myCanvas")).getContext("2d"); 
 		var img = document.getElementById("wagon");
-		ctx.drawImage(img, 0, position, 80, 30, 450, 70, 80, 30);
+		ctx.drawImage(img, 0, position, 80, 29, 450, 70, 120, 45);
 
 		// We increment the position by 30 each time
 		if (position < 30) { 
@@ -21,12 +26,32 @@ function animateScript($speed) {
 
 } //end of animateScript()
 
-function stopAnimate() {
+function stopAnimate(tID) {
 	clearInterval(tID);
 } //end of stopAnimate()
 
-// Testing Speed
-// 1 - Slow
-// 2 - Normal
-// 3 - Grueling
-//animateScript(2); 
+function animateLandmark(speed, landmark) {
+	speed = speed * 2;
+	var    position = 0; //start position for the image slicer
+	const  interval = 300/speed;
+	
+	tID2 = setInterval ( () => {
+		var c = document.getElementById("myCanvas");
+		var ctx = c.getContext("2d"); 
+		var img = document.getElementById("location");
+		if (landmark == "River") {
+			ctx.drawImage(img, 205, 58, 55, 23, position, 71, 90, 45);
+			ctx.fillStyle = "black";
+			ctx.fillRect(position-1, 107, speed+2, 8);
+		}
+		
+		// We increment the position by 30 each time
+		if (position < 360) { 
+			position = position + speed;
+		}
+		else {
+			stopAnimate(tID);
+		}
+		//reset the position to 0px, once position exceeds 30px
+	} ,interval ); //end of setInterval
+}
